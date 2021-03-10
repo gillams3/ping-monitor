9d911ad9b01b8dcf0edc2e9d056c8bc5ec1b9458
gillams3 <32503855+gillams3@users.noreply.github.com>
2021-03-09 22:54:41 -0600

    Update install-ping-monitor.sh
    remove backup config

--- install-ping-monitor.sh
+++ install-ping-monitor.sh
@@ -8,11 +8,4 @@ echo "Ping Monitor to toggle relay if no host are pingable"
 /bin/systemctl enable ping-monitor
 /usr/sbin/service ping-monitor restart
 
-echo "Automated backups into /root/pihole-backup"
-/usr/bin/install -m 744 -o root -d /root/pihole-backup
-cat > /tmp/$$ << EOF
-# Backup PiHole every day and delete old backups
-0 3 * * * root "cd /root/pihole-backup; /usr/local/bin/pihole -a -t; /usr/bin/find /root/pihole-backup -type f -name 'pi-hole-teleporter_*' -mtime- +150 -exec rm {} \;"
-EOF
-/usr/bin/install -m 644 -o root /tmp/$$ /etc/cron.d/pihole-backup
 echo "To view logs: # journalctl -fu ping-monitor"
